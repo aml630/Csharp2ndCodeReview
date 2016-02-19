1d87e6df1d6e446b417be4fe1ea40f1b61b51d4b
Alex <alex.m.larson@gmail.com>
2016-02-19 09:09:27 -0800

    no object version works with nancy

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,27 +1,24 @@
-// using Nancy;
-// using RepeatCounterNS.Objects;
-// using System.Collections.Generic;
-//
-// namespace RepeatCounterModuleNS
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ =>
-//       {
-//         return View["index.cshtml"];
-//       };
-//
-//       Post["/outputs"] = _ =>
-//       {
-//         string inputWord = Request.Form["word"];
-//         string inputPhrase = Request.Form["phrase"];
-//         RepeatCounter output = new RepeatCounter(inputWord, inputPhrase);
-//
-//         return View ["outputs.cshtml", output.CountRepeats()];
-//       };
-//
-//     }
-//   }
-// }
+using Nancy;
+using RepeatCounterNS.Objects;
+using System.Collections.Generic;
+
+namespace RepeatCounterModuleNS
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ =>
+      {
+        return View["index.cshtml"];
+      };
+
+      Post["/outputs"] = _ =>
+      {
+
+        return View ["outputs.cshtml", RepeatCounter.CountRepeats(Request.Form["word"], Request.Form["phrase"])];
+      };
+
+    }
+  }
+}
